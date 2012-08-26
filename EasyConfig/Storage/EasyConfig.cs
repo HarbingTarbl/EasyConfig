0d76994cc5c8e579a5eca85ea256b8af6d25f3a7
Michael Harris <mic2957@msn.com>
2012-08-26 18:27:37 -0400

    Added ability to save modified values back into config

--- EasyConfig/Storage/EasyConfig.cs
+++ EasyConfig/Storage/EasyConfig.cs
@@ -6,31 +6,39 @@ namespace EasyConfigLib.Storage
 	public abstract class EasyConfig
 		: Config
 	{
+		private readonly FieldInfo[] fields;
+
 		protected EasyConfig(string filename)
 			: base(filename)
 		{
 			Load();
+			fields = GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly | BindingFlags.Instance);
+			Array.Sort(fields, (f, s) => f.FieldHandle.Value.ToInt32().CompareTo(s.FieldHandle.Value.ToInt32()));
+			ReadValues();
+			Save();
+		}
+
+		public void ReadValues()
+		{
 			var t = GetType();
 			var lastSection = "";
-			var fields = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly | BindingFlags.Instance);
-			Array.Sort(fields, (f, s) => f.FieldHandle.Value.ToInt32().CompareTo(s.FieldHandle.Value.ToInt32()));
-
 			foreach (var field in fields)
 			{
-				var attrs = field.GetCustomAttributes(typeof(FieldAttribute), false);
+				var attrs = field.GetCustomAttributes(typeof (FieldAttribute), false);
 				if (attrs.Length == 0) continue;
 
-				var attr = (FieldAttribute)attrs[0];
+				var attr = (FieldAttribute) attrs[0];
 				var fieldName = attr.Name == "" ? field.Name : attr.Name;
 
-				if (attr.Section != "" && attr.Section != lastSection)
+				if (attr.Section != ""
+				    && attr.Section != lastSection)
 				{
 					lastSection = attr.Section;
 					SetSection(lastSection);
 				}
 
 				object value;
-				if(!Read(fieldName, out value, field.FieldType))
+				if (!Read(fieldName, out value, field.FieldType))
 				{
 					value = field.GetValue(this);
 					Write(fieldName, value);
@@ -38,9 +46,32 @@ namespace EasyConfigLib.Storage
 
 				field.SetValue(this, value);
 			}
-
-			Save();
 		}
+
+		public void WriteValues()
+		{
+			var t = GetType();
+			var lastSection = "";
+			foreach(var field in fields)
+			{
+				var attrs = field.GetCustomAttributes(typeof(FieldAttribute), false);
+				if (attrs.Length == 0) continue;
+
+				var attr = (FieldAttribute)attrs[0];
+				var fieldName = attr.Name == "" ? field.Name : attr.Name;
+				var fieldValue = field.GetValue(this);
+
+				if(attr.Section != "" &&
+					attr.Section != lastSection)
+				{
+					lastSection = attr.Section;
+					SetSection(lastSection);
+				}
+
+				Write(fieldName, fieldValue);
+			}
+		}
+
 
 
 		[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
